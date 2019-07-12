eaab34f4a7e53de89729a8d8a28ba86ceccc4774
Yangyang Cui <cuiyang21@gmail.com>
2019-07-12 17:57:49 +0800

    Having moved code, but the ship's turning didn't works well

--- Objects.js
+++ Objects.js
@@ -1,11 +1,3 @@
-function collision(obj1, obj2) {
-    return distance(obj1, obj2) < (obj1.radius + obj2.radius);
-}
-
-function distance(obj1, obj2) {
-    return Math.sqrt(Math.pow(obj1.x - obj2.x, 2) + Math.pow(obj1.y - obj2.y, 2));
-}
-
 function Mass(x, y, mass, radius, angle, x_speed, y_speed, rotate_speed) {
     this.x = x;
     this.y = y;
--- game.js
+++ game.js
@@ -1,161 +1,10 @@
-let canvas = document.getElementById("asteroid");
-let context = canvas.getContext("2d");
-let guide = true;
-let asteroids = [];
-let score = 0;
-let damaged_mass = 500;
-let asteroid_mass = 10000;
-let asteroid_force = 5000000;
-
-function create_asteroid() {
-    return new Asteroid(
-        canvas.width * Math.random(),
-        canvas.height * Math.random(),
-        asteroid_mass
-    );
-}
-// Mass.prototype.moving = function(angle, force, elapsed)
-// Mass.prototype.twist = function(force, elapsed) 
-function asteroid_live_moving(asteroid, elapsed) {
-    elapsed = elapsed || 0.016;
-    asteroid.moving((Math.PI * 2) * Math.random(), asteroid_force, elapsed)
-    asteroid.twist((Math.random() - 0.5) * asteroid_force * 0.01, elapsed);
+function collision(obj1, obj2) {
+    return distance(obj1, obj2) < (obj1.radius + obj2.radius);
 }
 
-function asteroid_live(elapsed) {
-    let asteroid = create_asteroid();
-    asteroid_live_moving(asteroid, elapsed);
-    return asteroid;
+function distance(obj1, obj2) {
+    return Math.sqrt(Math.pow(obj1.x - obj2.x, 2) + Math.pow(obj1.y - obj2.y, 2));
 }
-asteroids.push(asteroid_live());
-
-function asteroid_split(asteroid) {
-    asteroid.mass -= damaged_mass;
-    score += damaged_mass;
-    let split_rate = 0.25 + (Math.random - 0.5) * 0.5;
-    as1 = asteroid.child(asteroid.mass * split_rate);
-    as2 = asteroid.child(asteroid.mass * (1 - split_rate));
-    [as1, as2].forEach(function(child) {
-        if (child.mass <= damaged_mass) {
-            score += child.mass;
-        } else {
-            asteroids.push(child);
-            asteroid_live_moving(asteroid, elapsed);
-        }
-    })
-}
-// let asteroid = new Asteroid(
-//     canvas.width * Math.random(),
-//     canvas.height * Math.random(),
-//     10000);
-
-// asteroid.moving((Math.PI * 2) * Math.random(), 1000, 60)
-// asteroid.twist((Math.random() - 0.5) * 100, 60);
-
-let ship = new Ship(
-    canvas.width / 2,
-    canvas.height / 2,
-    10, 20, 1000, 200
-);
-let projectiles = [];
-
-// Control ship
-function key_handle(e, value) {
-    let handled_nothing = false;
-    switch (e.key || e.keyCode) {
-        case 38:
-        case "ArrowUp":
-            ship.go_forward = value;
-            break;
-        case 37:
-        case "ArrowLeft":
-            ship.turn_left = value;
-            break;
-        case 39:
-        case "ArrowRight":
-            ship.turn_right = value;
-            break;
-        case 40:
-        case "ArrowDown":
-            ship.go_backward = value;
-            break;
-        case 71:
-        case "g":
-            if (value) guide = !guide;
-            break;
-        case 32:
-        case " ":
-            ship.fire_on = value;
-            break;
-        default:
-            handled_nothing = true;
-    }
-    if (!handled_nothing) e.preventDefault();
-}
-canvas.addEventListener("keydown", function(e) {
-    key_handle(e, true);
-}, true);
-canvas.addEventListener("keyup", function(e) {
-    key_handle(e, false);
-}, true);
-canvas.focus();
-
-function draw() {
-    asteroids.forEach(function(asteroid, i, asteroids) {
-        projectiles.forEach(function(projectile, j, projectiles) {
-            draw_line(context, asteroid, projectile);
-        });
-        draw_line(context, asteroid, ship);
-    });
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    if (guide) {
-        draw_grid(context);
-    }
-    asteroids.forEach(function(asteroid) {
-        asteroid.draw(context, guide);
-    })
-    ship.draw(context, guide);
-    projectiles.forEach(function(projectile) {
-        projectile.draw(context);
-    });
-}
-
-function update(elapsed, canvas) {
-
-    asteroids.forEach(function(asteroid) {
-        asteroid.update(elapsed, canvas);
-    });
-    ship.update(elapsed, canvas);
-    projectiles.forEach(function(projectile, i) {
-        projectile.update(elapsed, canvas);
-        if (projectile.life <= 0) {
-            projectiles.splice(i, 1);
-        } else {
-            asteroids.forEach(function(asteroid, j) {
-                if (collision(asteroid, projectile)) {
-                    projectiles.splice(j, 1);
-                    asteroids.splice(j, 1);
-                    asteroid_split(asteroid);
-                }
-            })
-        }
-    });
-    if (ship.fire_on) {
-        projectiles.push(ship.projectile(elapsed));
-    }
-}
-
-let previous;
-
-function frame(timestamp) {
-    if (!previous) previous = timestamp;
-    let elapsed = timestamp - previous;
-    update(elapsed / 1000, canvas);
-    draw();
-    previous = timestamp;
-    window.requestAnimationFrame(frame);
-}
-window.requestAnimationFrame(frame);
 
 function AsteroidsGame(id) {
     this.canvas = document.getElementById(id);
@@ -166,7 +15,30 @@ function AsteroidsGame(id) {
     this.damaged_mass = 500;
     this.asteroid_mass = 10000;
     this.asteroid_force = 5000000;
-    this.asteroids.push(asteroid_live);
+    this.asteroids.push(this.asteroid_live());
+    this.ship_mass = 10;
+    this.move_power = 1000;
+    this.projectile_force = 200;
+    this.ship = new Ship(
+        this.canvas.width / 2,
+        this.canvas.height / 2,
+        this.ship_mass,
+        20,
+        this.move_power,
+        this.projectile_force
+    );
+    this.projectiles = [];
+    this.canvas.addEventListener("keydown", this.keyDown.bind(this), true);
+    this.canvas.addEventListener("keyup", this.keyUp.bind(this), true);
+    this.canvas.focus();
+    window.requestAnimationFrame(this.frame.bind(this));
+}
+
+AsteroidsGame.prototype.keyDown = function(e) {
+    this.key_handle(e, true);
+}
+AsteroidsGame.prototype.keyUp = function(e) {
+    this.key_handle(e, true);
 }
 
 AsteroidsGame.prototype.create_asteroid = function() {
@@ -189,8 +61,8 @@ AsteroidsGame.prototype.asteroid_live = function(elapsed) {
     return asteroid;
 }
 
-AsteroidsGame.prototype.asteroid_split = function(asteroid) {
-    this.asteroid.mass -= this.damaged_mass;
+AsteroidsGame.prototype.split_asteroid = function(asteroid, elapsed) {
+    this.asteroid_mass -= this.damaged_mass;
     this.score += this.damaged_mass;
     let split_rate = 0.25 + (Math.random() - 0.5) * 0.5;
     as1 = asteroid.child(this.asteroid_mass * split_rate);
@@ -203,4 +75,90 @@ AsteroidsGame.prototype.asteroid_split = function(asteroid) {
             this.asteroid_live_moving(child, elapsed);
         }
     }, this);
+}
+
+AsteroidsGame.prototype.key_handle = function(e, value) {
+    let handled_nothing = false;
+    switch (e.key || e.keyCode) {
+        case 38:
+        case "ArrowUp":
+            this.ship.go_forward = value;
+            break;
+        case 37:
+        case "ArrowLeft":
+            this.ship.turn_left = value;
+            break;
+        case 39:
+        case "ArrowRight":
+            this.ship.turn_right = value;
+            break;
+        case 40:
+        case "ArrowDown":
+            this.ship.go_backward = value;
+            break;
+        case 71:
+        case "g":
+            if (value) this.guide = !this.guide;
+            break;
+        case 32:
+        case " ":
+            this.ship.fire_on = value;
+            break;
+        default:
+            handled_nothing = true;
+    }
+    if (!handled_nothing) e.preventDefault();
+}
+
+AsteroidsGame.prototype.draw = function() {
+    this.c.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    if (this.guide) {
+        draw_grid(this.c);
+        this.asteroids.forEach(function(asteroid) {
+            draw_line(this.c, asteroid, this.ship);
+            this.projectiles.forEach(function(projectile) {
+                draw_line(this.c, asteroid, projectile);
+            }, this);
+        }, this);
+    }
+    this.asteroids.forEach(function(asteroid) {
+        asteroid.draw(this.c, this.guide);
+    }, this);
+    this.ship.draw(this.c, this.guide);
+    this.projectiles.forEach(function(projectile) {
+        projectile.draw(this.c);
+    }, this);
+}
+
+AsteroidsGame.prototype.update = function(elapsed) {
+    this.asteroids.forEach(function(asteroid) {
+        asteroid.update(elapsed, this.canvas);
+    }, this);
+    this.ship.update(elapsed, this.canvas);
+    this.projectiles.forEach(function(projectile, i) {
+        projectile.update(elapsed, this.canvas);
+        if (projectile.life <= 0) {
+            this.projectiles.splice(i, 1);
+        } else {
+            this.asteroids.forEach(function(asteroid, j) {
+                if (collision(projectile, asteroid)) {
+                    this.asteroids.splice(j, 1);
+                    this.projectiles.splice(i, 1);
+                    this.split_asteroid(asteroid);
+                }
+            }, this);
+        }
+    }, this);
+    if (this.ship.fire_on) {
+        this.projectiles.push(this.ship.projectile(elapsed));
+    }
+}
+
+AsteroidsGame.prototype.frame = function(timestamp) {
+    if (!this.previous) this.previous = timestamp;
+    let elapsed = timestamp - this.previous;
+    this.update(elapsed / 1000);
+    this.draw();
+    this.previous = timestamp;
+    window.requestAnimationFrame(this.frame.bind(this));
 }
